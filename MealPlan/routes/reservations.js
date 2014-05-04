ee30f4f4417e548a8da5648fe3f94f67bc601bd5
lastDeveloper <r_b_s@hotmail.fr>
2014-05-04 12:26:22 +0200

    add reservation

---
+++ MealPlan/routes/reservations.js
@@ -0,0 +1,105 @@
+var newErr = require('../error').newError;
+
+exports.createFromMeeting = function(req, res, next)
+{
+	var name = req.body.name;
+	var peopleNumber = req.body.peopleNumber;
+
+	req.db.models.reservation.create(
+	{
+		name: name,
+		peopleNumber : peopleNumber,
+		meeting_id : req.meeting.id,
+		owner_id : req.user.id,
+		restaurant_id : req.meeting.restaurant_id
+	}, function(err, reservation)
+	{
+		if (err)
+			next(newErr(500, err));
+		res.send(reservation);
+	});
+}
+
+exports.create = function(req, res, next)
+{
+
+	var name = req.body.name;
+	var peopleNumber = req.body.peopleNumber;
+	var restaurantId = req.body.restaurant_id;
+	req.db.models.reservation.create(
+	{
+		name: name,
+		peopleNumber : peopleNumber,
+		owner_id : req.user.id,
+		restaurant_id :restaurantId
+	}, function(err, reservation)
+	{
+		if (err)
+			next(newErr(500, err));
+		res.send(reservation);
+	});
+}
+
+exports.update = function(req, res, next)
+{
+	if (req.body.name)
+		req.reservation.name = req.body.name;
+	if (req.body.peopleNumber)
+		req.reservation.name = req.body.peopleNumber;
+	req.reservation.save(function(err, reservation)
+	{
+		console.log("err:" + err);
+				if (err)
+			next(newErr(500, err));
+		res.send(reservation);
+
+	});
+}
+
+exports.cancelByOwner = function(req, res, next)
+{
+	if (req.user.id != req.reservation.owner_id)
+	{
+		next(newErr(500,"authentification Error, user is not the owner"));
+		return ;
+	}
+	req.reservation.validationStatus = "cancelledByUser";
+	req.reservation.save(function(err, data)
+	{
+		if (err)
+			next(newErr(500, err));
+		res.send(data);
+	});
+}
+
+exports.validateByRestaurant = function(req, res, next)
+{
+	if (req.user.id != req.restaurant.id)
+	{
+		next(newErr(500, "authentification Error, restaurant is not the owner"));
+		return ;
+	}
+	req.reservation.validationStatus = "validateByRestaurant";
+	req.reservation.save(function(err, data)
+	{
+		if (err)
+			next(newErr(500, err));
+		res.send(data);
+	});
+}
+
+exports.cancelByRestaurant = function(req, res, next)
+{
+	if (req.user.id != req.restaurant.id)
+	{
+		next(newErr(500, "authentification Error, restaurant is not the owner"));
+		return ;
+	}
+	req.reservation.validationStatus = "cancelledByRestaurant";
+	req.reservation.save(function(err, data)
+	{
+		if (err)
+			next(newErr(500, err));
+		res.send(data);
+	});
+}
