92b52544e963ce7341d9f1089e5a01ce7d6251b5
lastDeveloper <r_b_s@hotmail.fr>
2014-05-05 12:09:13 +0200

    webApp route update

--- MealPlan/routes/webApp.js
+++ MealPlan/routes/webApp.js
@@ -17,12 +17,34 @@ exports.login = function(req, res, next)
 {
 	var userName = req.body.userName;
 	var password = req.body.password;
-
+	console.log("req.body");
+	console.log(req.body);
 	req.session.userName = userName;
 	req.session.password = password;
 	res.redirect('/webApp');
 }
 
+exports.signUp = function(req, res, next)
+{
+	var userName = req.body.userName;
+	var password = req.body.password;
+	var name = req.body.name;
+	var password = req.body.password;
+	var category = req.body.category;
+
+	req.db.models.restaurant.create(
+	{
+		userName : userName,
+		password : password,
+		name : name,
+		category : category
+	}, function(err, restaurant)
+	{
+		if (err)
+			next(newErr(err));
+		res.send(restaurant);
+	});
+}
 exports.loginPage = function(req, res, next)
 {
 	res.render('login');
