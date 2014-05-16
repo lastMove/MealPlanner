7e458456d962c1a27ac46f53e6ee2282a649f15d
lastDeveloper <r_b_s@hotmail.fr>
2014-05-16 11:16:52 +0200

    add console log to app.js

--- MealPlan/app.js
+++ MealPlan/app.js
@@ -71,6 +71,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') 
 {
     app.use(function(err, req, res, next) {
+        console.log(err);
         res.status(err.status || 501);
         res.render('error', {
             message: err.message,
@@ -82,6 +83,8 @@ if (app.get('env') === 'development')
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
+            console.log(err);
+
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
