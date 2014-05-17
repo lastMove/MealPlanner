d62fcacfee5e15bf7b462a9036a55615399682fb
Charles Censier <charles.censier@gmail.com>
2014-05-17 14:59:19 +0800

    koukou lol

--- MealPlan/routes/stats.js
+++ MealPlan/routes/stats.js
@@ -4,3 +4,19 @@ exports.statsPage = function(req, res, next)
 {
 	res.render('stats');
 }
+
+topCustomers(req, res, next, topDishes)
+{
+	req.db.models.order.find({restaurant_id : req.restaurant.id}, function(err, orders)
+	{
+		if (err)
+			res.render('error', {
+				message: err.message,
+				error: err
+			});
+		else
+		{
+			
+		}
+	});
+}
